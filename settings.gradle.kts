1cab74a8f7160ec5cd29638017d290b89b6cd02c
yorik100 <yorik100@hotmail.com>
2026-08-29 20:34:38 +0200

    Errors fixed 2

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,6 +3,9 @@
 pluginManagement {
     buildscript {
         repositories {
+            maven {
+                url = uri("https://storage.googleapis.com/r8-releases/raw")
+            }
             google()
             mavenCentral()
         }
