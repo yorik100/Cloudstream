3509a77566814509498fc3fe11e90838ae03555b
yorik100 <yorik100@hotmail.com>
2026-09-04 13:53:01 +0200

    Add files via upload

--- AfterDark/build.gradle.kts
+++ AfterDark/build.gradle.kts
@@ -1,10 +1,10 @@
-version = 26
+version = 27
 cloudstream {
     description = "AfterDark - récupération des sources après vérification officielle dans WebView"
     authors = listOf("yorik100")
     status = 3
     tvTypes = listOf("Movie", "TvSeries")
     language = "fr"
-    iconUrl = "https://www.google.com/s2/favicons?domain=afterdark06.mom&sz=%size%"
+    iconUrl = "https://www.google.com/s2/favicons?domain=afd926.mom&sz=%size%"
     isCrossPlatform = false
 }
