a536ef91ab8f10baccf035028f4d85dd4164e05d
yorik100 <yorik100@hotmail.com>
2026-09-21 01:37:42 +0200

    Update build.gradle.kts

--- AfterDark/build.gradle.kts
+++ AfterDark/build.gradle.kts
@@ -2,7 +2,7 @@ import com.lagradost.cloudstream3.gradle.tasks.CompileDexTask
 import org.gradle.api.attributes.Attribute
 import org.gradle.api.tasks.Sync
 
-version = 94
+version = 95
 
 val publishedIconBase =
     "https://raw.githubusercontent.com/" +
